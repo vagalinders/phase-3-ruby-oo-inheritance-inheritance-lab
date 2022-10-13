cd3b9c3e6c89c0ba4cd02dd7072286516c3306a2
Chris Mann <chrispmann@gmail.com>
2022-10-12 17:09:47 -0700

    commit tree 02de134f93a58429be92ffeea0262e0e52cd4f32

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge.push(string)
+    end
+
+    def knowledge
+        @knowledge
+    end
+
 
 end
